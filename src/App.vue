4c836d41dd413bc397b05f796c8f0cc4dd29d95b
maizhan <wx199703223438@outlook.com>
2023-09-26 19:38:09 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './styles/dark/css-vars.css'
+</script>
+
+<template>
+  <header>
+    <!--    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125"/>-->
+    <title>MYJO-OPEN-API</title>
+  </header>
+  <body>
+    <div id="app">
+      <template>
+        <div class="common-layout">
+          <el-container>
+            <el-header>Header</el-header>
+            <el-container>
+              <el-aside width="200px">Aside</el-aside>
+              <el-main>Main</el-main>
+            </el-container>
+          </el-container>
+        </div>
+      </template>
+    </div>
+  </body>
+  <!--  <RouterView/>-->
+</template>
+
